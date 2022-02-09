00f5245894efca2adf81e9d3e0be58527cb92fde
Imad Abdou <igentle.appletec@gmail.com>
2022-02-09 13:33:28 +0100

    Check if site is down or login info not valide

--- etms_utilities/api.py
+++ etms_utilities/api.py
@@ -1,4 +1,6 @@
 import frappe
+from frappe import _
+from frappe import frappeclient
 from frappe.frappeclient import FrappeClient
 
 @frappe.whitelist()
@@ -8,9 +10,11 @@ def etms_site_admin_login(docname):
     site_user = etms_site.get("site_manager_user")
     site_pass = etms_site.get_password("site_manager_pass")
 
-    client = FrappeClient(url=f"https://{etms_site.name}", username=site_user, password=site_pass, verify=True)
+    try:
+        client = FrappeClient(url=f"https://{etms_site.name}", username=site_user, password=site_pass, verify=True)
+        sid = client.session.cookies['sid']
 
+        return sid
+    except:
+        frappe.msgprint(_("Login Info incorrect or site is down"))
 
-    sid = client.session.cookies['sid']
-
-    return sid
--- etms_utilities/public/js/etms_erp_site.js
+++ etms_utilities/public/js/etms_erp_site.js
@@ -1,12 +1,18 @@
 frappe.ui.form.on("ETMS ERP Site", {
     refresh: function(frm) {
         frm.add_custom_button(frappe._("Login As Site Manager"), function(){
+            if(!frm.doc.site_manager_user || !frm.doc.site_manager_pass) {
+                frappe.msgprint(frappe._("Please set (Site Manager User) and (Site Manager Password) first."), "ETMS Utilities");
+                return;
+            }
             frappe.call({
                 method: "etms_utilities.api.etms_site_admin_login",
                 args: {docname: frm.docname},
                 callback: function(r) {
-                    var site_url = "https://" + frm.doc.name + "/desk?sid=" + r.message;
-                    window.open( site_url, "_blank").focus();
+                    if(r.message) {
+                        var site_url = "https://" + frm.doc.name + "/desk?sid=" + r.message;
+                        window.open( site_url, "_blank").focus();
+                    }
                 }
             })
         })
